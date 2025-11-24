a0f5a52b906b4277a27240ab4a52cc460ac19ed7
Farhadzafar <farhadzafari10@gmail.com>
2025-11-24 14:43:04 +0430

    test fix the test test of the untitetesing

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,5 +7,5 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 19)
 
     def testCalc1(self):
-        res = calc.add(9, 10)
-        self.assertEqual(res, 29)
+        res = calc.add(9, 90)
+        self.assertEqual(res, 99)
